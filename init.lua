c781a4e7fff4fa7639996a948dd7ca39d1e1c0a9
Nick Pappas <nick.pappas.ee@gmail.com>
2025-06-21 22:26:21 +0300

    Add options

--- init.lua
+++ init.lua
@@ -1,1 +1,11 @@
+-- Set leader key before plugins are loaded
+vim.g.mapleader = " "
+vim.g.maplocalleader = "\\"
+
+-- Load core editor options
+require("config.options")
+
+-- (Optional but recommended) Load global keymaps
+-- require("config.keymaps")
+
 require("config.lazy")
